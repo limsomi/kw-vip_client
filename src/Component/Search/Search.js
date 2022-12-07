8539f90ecfd7117365d4a7dd5640004ed6ac6782
limsomi <97082128+limsomi@users.noreply.github.com>
2022-12-07 14:44:00 +0900

    Update Search.js

--- src/Component/Search/Search.js
+++ src/Component/Search/Search.js
@@ -1,12 +1,23 @@
 import React, {useState,useEffect} from "react";
 import './Search.css';
 import Map from "../../map";
+import estimated_rent from './estimated_rent';
+import estimated_sales from './estimated_sales';
+import data_e from './sales&rent.json';
 const SearchBar=()=>{
     const [search,setSearch]=useState(""); 
     const [data_print,setData]=useState("");
     const [business,setBusiness]=useState("");
     const [floor,setFloor]=useState("");
     const [paint,setPaint]=useState("");
+    const [person_cost,setPerson]=useState("");
+    const [admin_cost,setAdmin]=useState("");
+    const [local,setLocal]=useState("");
+    const [price,setPrice]=useState("");
+    const [rent,setRent]=useState("");
+    const [total_cost,setTotal]=useState("");
+    const [showPopup,setShowPopup]=useState(false);
+    const [showTotal,setShowTotal]=useState(false);
     
     var data_value="";
 
@@ -36,15 +47,55 @@ const SearchBar=()=>{
         event.preventDefault();
         setSearch(event.target.value);
     }
+
+    const onGetdata=(local_name,price,e)=>{
+        console.log(local_name);
+        console.log(price);
+        setLocal(local_name);
+        setPrice(price);
+        {showPopup === false ? setShowPopup(true) :setShowPopup(false)}
+    }
+
+
     const onClick=async()=>{
-        data_value=Content(search);
+        data_value=Content(search,floor,business);
         var data_list=data_value.map((local) => 
-        <Print key={local.id} ranking={local.ranking} local_name={local.local_name} price={local.price}></Print>);
+        <div id="local_check" value='true' onClick={(e)=>{onGetdata(local.local_name,local.price,e)}} key={local.id} local_name={local.local_name} price={local.price}>{local.ranking}위 {local.local_name} {local.price}</div>);
+        
         
         setData(data_list);
         setPaint(data_value);
     }
 
+    const onCal=async()=>{
+        
+        let rent_e=data_e.value;
+        console.log(local);
+        for(let i = 0; i <rent_e.length ; i++){
+            let location=rent_e[i].구+" "+rent_e[i].동
+            if(local==location)
+            {
+                setRent(Number(rent_e[i].추정월세));
+                break;
+            }
+        }
+        console.log(rent);
+        const total_cost=Number(price)-Number(admin_cost)-Number(person_cost)-Number(rent);
+        setTotal(total_cost);
+        console.log(total_cost);
+        {showTotal===false?setShowTotal(true):setShowTotal(false)}
+        
+    }
+    const onChangePerson=event=>{
+        event.preventDefault();
+        setPerson(event.target.value);
+    }
+    const onChangeAdmin=event=>{
+        event.preventDefault();
+        setAdmin(event.target.value);
+    }
+
+   
     
     return(
         <>
@@ -53,8 +104,10 @@ const SearchBar=()=>{
             <div className='title'>상권분석</div>
             <div className='component_checkbox'>
                 <div className='button'>
+                    <button id={"btn"+(floor==="B1" ?"_acitve" :"")} value="B1" onClick={onFloor}>B1</button>
                     <button id={"btn"+(floor==="1F" ?"_acitve" :"")} value="1F" onClick={onFloor}>1F</button>
                     <button id={"btn"+(floor==="2F" ?"_acitve" :"")} value="2F" onClick={onFloor}>2F</button>
+                    <button id={"btn"+(floor==="3F" ?"_acitve" :"")} value="3F" onClick={onFloor}>3F</button>
                 </div>
                 <div className='button'>
                     <button id={"btn"+(business==="음식점" ?"_acitve" :"")} value="음식점" onClick={onBusiness}>음식점</button>
@@ -76,15 +129,46 @@ const SearchBar=()=>{
                     <div className='price'>지역 별 순위....</div>
                     <div id='data'>{data_print}</div>
                 </div>
+
         </div>
         </div>
+            {showPopup?(
+        <div className="sub_window">
+            <div className='title'>상권분석</div>
+            <button id="btn_back" onClick={onGetdata}>뒤로가기</button>
+            <input id="inputbox2"
+                type="search"
+                value={person_cost}
+                placeholder="인건비"
+                onChange={onChangePerson}
+                />
+            <input id="inputbox2"
+                type="search"
+                value={admin_cost}
+                placeholder="관리비"
+                onChange={onChangeAdmin}
+                />
+            <button id="search_button" value='false' onClick={onCal}>계산하기</button>
+            <hr></hr>
+            {showTotal?(<div id='total_cal'>
+                <div id='cal_title'>인건비가 {person_cost}이고 ,관리비가 {admin_cost},</div>
+                <div id='cal_title'>월세가 {rent} 일 때</div>
+                <div id='cal_title'>{local}의 상권 최대 이익은 </div>
+                <p>
+                    <span id='total_cost'>{total_cost}</span>
+                    <span id='cal_title'>원 입니다</span>
+                </p>
+            </div>):null}
+        </div>
+        )
+        :null}
 
         </>
     )
 }
 export default SearchBar;
 
-const data=[//test값
+let data=[//test값
     {
         area:25,
         value:[
@@ -93,7 +177,7 @@ const data=[//test값
             {id:3,ranking:3,local_name:"서대문구 홍제1동",price:'200'},
             {id:4,ranking:4,local_name:"중구 명동",price:'500'},
             {id:5,ranking:5,local_name:"용산구 남영동",price:'300'}
-        ]
+            ]
        
     },
     {
@@ -104,7 +188,7 @@ const data=[//test값
             {id:3,ranking:3,local_name:"종로구 무악동",price:'200'},
             {id:4,ranking:4,local_name:"성북구 안암동",price:'500'},
             {id:5,ranking:5,local_name:"노원구 상계1동",price:'300'}
-        ]
+            ]
        
     },
     {
@@ -121,10 +205,15 @@ const data=[//test값
 
 ]
 
-function Content(search){
+
+function Content(search,floor,business){
+    for(let i = 0; i < data.length ; i++){
+        data[i].value = max_data(business);
+
+    }
     var data_value="";
     for(var i in data){
-        if(data[i]['area']==search)
+        if(data[i]['area']== search)
         {
             data_value=data[i]['value'];
             break;
@@ -132,37 +221,71 @@ function Content(search){
     }
 
     if(data_value===""){return("Not found data")}
-    
+    console.log(data_value);
     return(
        data_value
-
     )
 
 }
 
-function Print({ranking,local_name,price}){
-    let local_price=ranking+"위 "+local_name+" "+price
-    const [showPopup,setShowPopup]=useState("");
-    const onGetdata=(e)=>{
-        const {value}=e.target;
-        console.log(typeof(value));
-        if(showPopup!=e.target.value)
-        {
-            
-            setShowPopup(e.target.value);
+
+
+function max_data(business){
+    let maxvalue = [];
+    let endList = [];
+
+    while(endList.length !== 5){
+        let max = 0;
+        let maxlocation = "";
+        let sales=data_e.value;
+        // for(let i = 0; i < market.length ; i++){
+        //     let location = market[i].구 + " " + market[i].동;
+
+        //     if(!endList.includes(location)){
+        //         if(max <  market[i].추정월세){
+        //             max = market[i].추정월세;
+        //             maxlocation = location;
+        //         }
+        //     }
+        // }
+        for(let i = 0; i < sales.length ; i++){
+            if(business==sales[i].업종)
+            {
+                let location = sales[i].구 + " " + sales[i].동;
+                if(!endList.includes(location)){
+                    if(max <  sales[i].점포당추정매출){
+                        max = sales[i].점포당추정매출;
+                        maxlocation = location;
+                    }
+                }
+            }
         }
-        else{
-            setShowPopup("");
-        }
-        console.log(showPopup);
-        // console.log(typeof(showPopup));
+
+        maxvalue.push({id:maxvalue.length + 1, ranking: maxvalue.length + 1, local_name: maxlocation, price: max});
+        endList.push(maxlocation);
     }
-   return(
-    <>
-        <div id="local_check" onClick={onGetdata} value='false'>{local_price}</div>
-        {showPopup?(
-            <div className="sub_window">hi</div>
-        ):null}
-    </>
-   )
+
+    return maxvalue;
 }
+
+// function min_data(){
+//     let minvalue = [];
+//     let endList = [];
+
+//     while(minvalue.length !== 5){
+//         let min = 0;
+//         let minlocation = "";
+//         for(let i = 0; i < market.length ; i++){
+//             let location = market[i].구 + " " + market[i].동;
+
+//             if(!endList.includes(location)){
+//                 if(min >  market[i].추정월세){
+//                     min = market[i].추정월세;
+//                     minlocation = location;
+//                 }
+//             }
+//         }
+
+//         minvalue.push({id:minvalue.length + 1, ranking: minvalue.length + 1, local_name: minlocation, price: min});
+//     }
+// }
